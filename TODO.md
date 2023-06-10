93cf8319018b0535eb43703a96b1d15ff004b940
asierzapata <asier.zapata@gmail.com>
2023-06-10 11:51:49 +0200

    Convert into monorepo

--- TODO.md
+++
@@ -1,4 +0,0 @@
-- [] Create UI components to centralize dark and light mode
-  One example could be tha screen layout.
-
-https://wix.github.io/react-native-calendars/
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,13 +1,38 @@
-/** @type {import("prettier").Config} */
-module.exports = {
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+	arrowParens: "always",
 	printWidth: 80,
-	useTabs: true,
-	semi: false,
-	singleQuote: true,
+	singleQuote: false,
 	jsxSingleQuote: false,
-	quoteProps: "as-needed",
-	trailingComma: "none",
-	bracketSpacing: true,
-	jsxBracketSameLine: false,
-	arrowParens: "avoid",
+	semi: true,
+	trailingComma: "all",
+	tabWidth: 2,
+	// pluginSearchDirs: false,
+	plugins: [
+		"@ianvs/prettier-plugin-sort-imports",
+		"prettier-plugin-tailwindcss",
+	],
+	tailwindConfig: "./packages/config/tailwind",
+	importOrder: [
+		"^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+		"^(next/(.*)$)|^(next$)",
+		"^(expo(.*)$)|^(expo$)",
+		"<THIRD_PARTY_MODULES>",
+		"",
+		"^@taskly/(.*)$",
+		"",
+		"^~/utils/(.*)$",
+		"^~/components/(.*)$",
+		"^~/styles/(.*)$",
+		"^~/(.*)$",
+		"^[./]",
+	],
+	importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+	importOrderTypeScriptVersion: "5.0.4",
 };
+
+module.exports = config;
