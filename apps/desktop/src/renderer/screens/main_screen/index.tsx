3cfb7f0257f1a3f4d0ff37687845edd0a02c02da
asierzapata <asier.zapata@gmail.com>
2023-06-17 09:33:23 +0200

    (Desktop) Add split panes to main screen

--- apps/desktop/src/renderer/screens/main_screen/index.tsx
+++ apps/desktop/src/renderer/screens/main_screen/index.tsx
@@ -21,8 +21,10 @@ import {
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { TextEditor } from '@renderer/text_editor'
+import SplitPane from 'react-split-pane'
 
 import './titlebar.css'
+import './split-pane.css'
 
 /* ====================================================== */
 /*                    Implementation                     */
@@ -35,100 +37,109 @@ export const MainScreen = () => {
 				<div className="titlebar-text">Taskly</div>
 			</div>
 			<div className="min-h-screen-without-frame flex w-full">
-				<div className="max-h-screen-without-frame h-screen max-w-xs">
-					<div className="relative flex h-full flex-col items-center justify-between rounded-lg shadow-md">
-						<div className="w-full p-3">
-							<Button
-								variant="ghost"
-								className={classnames(
-									'flex w-full items-center justify-start gap-3 px-0'
-								)}
-								// onClick={onToggle}
-							>
-								<Icons.chevronLeft size={20} />
-								<span className="flex items-center gap-3 ">
-									<span className="bg-gradient-to-tl from-amber-400 to-orange-600 bg-clip-text text-transparent">
-										Taskly
-									</span>
-								</span>
-							</Button>
-						</div>
-						<div className="w-full flex-1 overflow-y-auto p-3">
-							<nav aria-label="Main Nav" className="flex flex-col space-y-1">
-								<Link to={`/`}>
-									<Button
-										as="a"
-										variant="ghost"
-										className={classnames(
-											'flex w-full items-center gap-3',
-											'justify-start'
-										)}
-										// onClick={isSmallViewport ? onToggle : _.noop}
-									>
-										<Icons.calendarClock size={20} />
-										{/* {isOpen ? ( */}
-										<span className="text-sm font-medium"> Today </span>
-										{/* ) : null} */}
-									</Button>
-								</Link>
-							</nav>
-							<div className="mh-4 mv-6 h-0.5 w-full bg-gradient-to-tl from-amber-400 to-orange-600" />
-
-							<div className="mt-5">
-								<Collapsible>
-									<span
-										className={classnames(
-											'flex items-center justify-between rounded px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-800',
-											Math.random() > 0.5 && 'bg-slate-100 dark:bg-slate-800'
-										)}
-									>
-										<Link to={`/`} className="w-full">
-											<span className="ml-4 flex-1 text-sm">
-												This is an Area!
-											</span>
-										</Link>
-
-										<CollapsibleTrigger asChild>
-											<Button
-												variant="ghost"
-												size="xs"
-												className="data-[state=open]:bg-slate-200 data-[state=open]:dark:bg-slate-700 [&[data-state=open]>svg]:rotate-180"
-											>
-												<Icons.chevronDown
-													size={14}
-													className="transition-transform duration-200"
-												/>
-											</Button>
-										</CollapsibleTrigger>
-									</span>
-									<CollapsibleContent>
-										<Button
-											as="a"
-											variant="ghost"
-											size="xs"
-											className={classnames(
-												'flex items-center justify-between rounded px-4 py-4 hover:bg-slate-200 dark:hover:bg-slate-800',
-												Math.random() > 0.5 && 'bg-slate-200 dark:bg-slate-800'
-											)}
-										>
-											<span className="ml-4 flex-1 text-xs">
-												This is a project!
-											</span>
-										</Button>
-									</CollapsibleContent>
-								</Collapsible>
-							</div>
-						</div>
-					</div>
+				<SplitPane split="vertical" defaultSize={250} maxSize={300}>
+					<SideBar />
+					<SplitPane split="vertical" primary="second" defaultSize="25%">
+						<Editor />
+						<Calendar />
+					</SplitPane>
+				</SplitPane>
+				{/* <div className="max-h-screen-without-frame h-screen max-w-xs">
+					<SideBar />
 				</div>
 				<div className="max-h-screen-without-frame relative flex-1 overflow-y-auto">
 					<Editor />
 				</div>
 				<div className="max-h-screen-without-frame relative w-[350px] max-w-[400px] overflow-y-auto p-3 pb-6">
 					<Calendar />
-				</div>
+				</div> */}
 			</div>
 		</>
+	)
+}
+
+function SideBar() {
+	return (
+		<div className="relative flex h-full flex-col items-center justify-between rounded-lg shadow-md">
+			<div className="w-full p-3">
+				<Button
+					variant="ghost"
+					className={classnames(
+						'flex w-full items-center justify-start gap-3 px-0'
+					)}
+					// onClick={onToggle}
+				>
+					<Icons.chevronLeft size={20} />
+					<span className="flex items-center gap-3 ">
+						<span className="bg-gradient-to-tl from-amber-400 to-orange-600 bg-clip-text text-transparent">
+							Taskly
+						</span>
+					</span>
+				</Button>
+			</div>
+			<div className="w-full flex-1 overflow-y-auto p-3">
+				<nav aria-label="Main Nav" className="flex flex-col space-y-1">
+					<Link to={`/`}>
+						<Button
+							as="a"
+							variant="ghost"
+							className={classnames(
+								'flex w-full items-center gap-3',
+								'justify-start'
+							)}
+							// onClick={isSmallViewport ? onToggle : _.noop}
+						>
+							<Icons.calendarClock size={20} />
+							{/* {isOpen ? ( */}
+							<span className="text-sm font-medium"> Today </span>
+							{/* ) : null} */}
+						</Button>
+					</Link>
+				</nav>
+				<div className="mh-4 mv-6 h-0.5 w-full bg-gradient-to-tl from-amber-400 to-orange-600" />
+
+				<div className="mt-5">
+					<Collapsible>
+						<span
+							className={classnames(
+								'flex items-center justify-between rounded px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-800',
+								Math.random() > 0.5 && 'bg-slate-100 dark:bg-slate-800'
+							)}
+						>
+							<Link to={`/`} className="w-full">
+								<span className="ml-4 flex-1 text-sm">This is an Area!</span>
+							</Link>
+
+							<CollapsibleTrigger asChild>
+								<Button
+									variant="ghost"
+									size="xs"
+									className="data-[state=open]:bg-slate-200 data-[state=open]:dark:bg-slate-700 [&[data-state=open]>svg]:rotate-180"
+								>
+									<Icons.chevronDown
+										size={14}
+										className="transition-transform duration-200"
+									/>
+								</Button>
+							</CollapsibleTrigger>
+						</span>
+						<CollapsibleContent>
+							<Button
+								as="a"
+								variant="ghost"
+								size="xs"
+								className={classnames(
+									'flex items-center justify-between rounded px-4 py-4 hover:bg-slate-200 dark:hover:bg-slate-800',
+									Math.random() > 0.5 && 'bg-slate-200 dark:bg-slate-800'
+								)}
+							>
+								<span className="ml-4 flex-1 text-xs">This is a project!</span>
+							</Button>
+						</CollapsibleContent>
+					</Collapsible>
+				</div>
+			</div>
+		</div>
 	)
 }
 
