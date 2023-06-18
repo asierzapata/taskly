c4077c34c388d3a138a7ccf78d2ae80dbe730651
asierzapata <asier.zapata@gmail.com>
2023-06-18 16:32:31 +0200

    (CI) Fix type-check turbo task

--- apps/mobile/index.tsx
+++ apps/mobile/index.tsx
@@ -1,10 +1,11 @@
-import { registerRootComponent } from 'expo'
 import { ExpoRoot } from 'expo-router'
+import { registerRootComponent } from 'expo'
+import { type RequireContext } from 'expo-router/build/types'
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
 	const ctx = require.context('./src/app')
-	return <ExpoRoot context={ctx} />
+	return <ExpoRoot context={ctx as RequireContext} />
 }
 
 registerRootComponent(App)
