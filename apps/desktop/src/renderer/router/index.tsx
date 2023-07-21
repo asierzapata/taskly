6a151101f761b6877a5d43cb0625a15f3d02565e
asierzapata <asier.zapata@gmail.com>
2023-07-21 08:11:21 +0200

    (Desktop) Comment authentication for now

--- apps/desktop/src/renderer/router/index.tsx
+++ apps/desktop/src/renderer/router/index.tsx
@@ -7,7 +7,8 @@ import {
 	RouterProvider
 } from 'react-router-dom'
 
-import { AuthenticationWrapper } from './authentication_wrapper'
+// For now we don't need authentication
+// import { AuthenticationWrapper } from './authentication_wrapper'
 import { SafeSelection } from '@renderer/screens/safe_selection'
 import { SafeCreation } from '@renderer/screens/safe_creation'
 import { SafeLayout } from '@renderer/screens/safe_layout'
@@ -17,14 +18,14 @@ import { Note } from '@renderer/screens/note'
 const appRouter = createHashRouter(
 	createRoutesFromElements(
 		<>
-			<Route element={<AuthenticationWrapper />}>
-				<Route path="/" element={<SafeSelection />} />
-				<Route path="/create_safe" element={<SafeCreation />} />
-				<Route path="/safe/:safeId" element={<SafeLayout />}>
-					<Route path="" element={<SafeWelcome />} />
-					<Route path="note/:noteId" element={<Note />} />
-				</Route>
+			{/* <Route element={<AuthenticationWrapper />}> */}
+			<Route path="/" element={<SafeSelection />} />
+			<Route path="/create_safe" element={<SafeCreation />} />
+			<Route path="/safe/:safeId" element={<SafeLayout />}>
+				<Route path="" element={<SafeWelcome />} />
+				<Route path="note/:noteId" element={<Note />} />
 			</Route>
+			{/* </Route> */}
 		</>
 	)
 )
